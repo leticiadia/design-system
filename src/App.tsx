9ae060f9afd14c79260eb6e2022c7b42bb5b027e
Leticia Dias <contatoleticiadia@gmail.com>
2022-10-12 22:49:02 -0300

    feat: initial structure of the created component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './styles/global.css';
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Hello World!</h1>
+  )
+}
+
